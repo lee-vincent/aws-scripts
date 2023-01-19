fecf168e42eb88e7c334ebfb63ee74406e011245
lee-vincent <vinnie@vinnielee.io>
2023-01-19 10:28:34 -0500

    handle None instancen ids

--- get-all-ebs.sh
+++ get-all-ebs.sh
@@ -1,21 +1,5 @@
 #! /bin/bash
 
-set -eo pipefail
-
-trap cleanup EXIT
-
-cleanup() {
-    exit_code=$?
-    if [[ ${exit_code} -eq 252 ]]; then
-       echo -e "\t***error: did you set your aws region and profile correctly?***"
-     elif [[ ${exit_code} -eq 255 ]]; then
-       echo -e "  ***error: did you set your aws region and profile correctly?***"
-    fi
-    if [ "$exit_code" -ne "0" ]; then
-        print_help $exit_code
-    fi
-}
-
 print_help() {
     echo Usage:
     echo -e "  get-all-ebs region|help [aws-profile]"
@@ -29,7 +13,14 @@ elif [[ ! -z "$2" ]]; then
 fi
 
 _region="$1"
-
+_regional_instances_file="/tmp/instances-$_region.txt"
+_sorted_regional_instances_file="/tmp/sorted-instances-$_region.txt"
+_regional_vpcs_subnets_file="/tmp/instances-vpc-subnet-$_region.txt"
+_regional_ebs_volumes_file="/tmp/ebs-volume-details-$_region.txt"
+echo -n > $_regional_instances_file
+echo -n > $_sorted_regional_instances_file
+echo -n > $_regional_vpcs_subnets_file
+echo -n > $_regional_ebs_volumes_file
 # get all instance ids, their AZ, volumes and volume sizes saved to /tmp/instances-$_region.txt
 # us-east-1a      i-0259717a7d755a9c8     vol-0d1b71c3a58d8ef82   8
 # us-east-1a      i-0259717a7d755a9c8     vol-06cf3217eb695745a   8
@@ -37,18 +28,24 @@ _region="$1"
 # us-east-1a      i-04168adbd4456da39     vol-089522b279fbe12d5   5
 # us-east-1a      i-04168adbd4456da39     vol-0a7b9c4f4bb34fe73   8
 # us-east-1a      i-04168adbd4456da39     vol-090d2168c63491af6   8
-aws ec2 describe-volumes --output text --query 'Volumes[*].[AvailabilityZone, Attachments[0].InstanceId, VolumeId, Size]' $_profile --region $_region > /tmp/instances-$_region.txt
+aws ec2 describe-volumes --output text --query 'Volumes[*].[AvailabilityZone, Attachments[0].InstanceId, VolumeId, Size]' $_profile --region $_region > $_regional_instances_file
+sort --reverse $_regional_instances_file > $_sorted_regional_instances_file
+
 # put all the instance ids into a string variable
-aws_instances="$(cat /tmp/instances.txt | cut -f 2)"
+aws_instances="$(cat $_sorted_regional_instances_file | grep -Ev "None" | cut -f 2)"
 
 # convert the string variable to an array
 aws_instances_array=($aws_instances)
 
-# get the VpcId and SubnetId for each instance id in the aws_instances_array saved to /tmp/instances_vpc_subnet-$_region.txt
-echo -n > /tmp/instances_vpc_subnet-$_region.txt # just make sure it is empty since next command appends using >> redirection
-for (( i=0; i<${#aws_instances_array[@]}; i++ )); do aws ec2 describe-instances --instance-ids "${aws_instances_array[$i]}" --output text --query 'Reservations[*].Instances[*].[VpcId,SubnetId]' $_profile --region $_region >> /tmp/instances_vpc_subnet-$_region.txt; done
-
+if [ ${#aws_instances_array[@]}  -ne 0 ]; then
+    # get the VpcId and SubnetId for each instance id in the aws_instances_array saved to /tmp/instances_vpc_subnet-$_region.txt
+    for (( i=0; i<${#aws_instances_array[@]}; i++ )); do aws ec2 describe-instances --instance-ids "${aws_instances_array[$i]}" --output text --query 'Reservations[*].Instances[*].[VpcId,SubnetId]' $_profile --region $_region >> $_regional_vpcs_subnets_file; done
+fi
 # combine the columns from /tmp/instances-$_region.txt and /tmp/instances_vpc_subnet-$_region.txt into /tmp/ebs-volume-details-$_region.txt
-paste /tmp/instances-$_region.txt /tmp/instances_vpc_subnet-$_region.txt > /tmp/ebs-volume-details-$_region.txt
+paste $_sorted_regional_instances_file $_regional_vpcs_subnets_file > $_regional_ebs_volumes_file
+
+rm -rf $_regional_vpcs_subnets_file $_sorted_regional_instances_file $_regional_instances_file
+
+echo "results located in $_regional_ebs_volumes_file" 
 
 exit 0
